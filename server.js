fcd8cc1f6d5773021679ddfb140e865dd20aedd5
LilianMathu <lilianmathu2018@gmail.com>
2021-04-16 11:41:29 +0300

    Revert "project/project setup"
    This reverts commit 0861f50b75ef13b5f28827afa6e3949537ac2ca0.

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const config = require('./Config');
-
-const port = config.PORT;
-
-app.listen(port, ()=> {
-    console.log(`Server is and running on port ${process.env.PORT}`);
-});
-;
