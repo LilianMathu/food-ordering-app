d261988a26123afec1474fbc90399df5b7276caa
LilianMathu <lilianmathu2018@gmail.com>
2021-04-17 01:20:09 +0300

    feature/2: Implement get menu items

--- routes/menuroute.js
+++ routes/menuroute.js
@@ -29,4 +29,33 @@ router.post('/', (req, res) => {
         });
 });
 
+
+// Menu GET routes
+router.get('/', (req, res) => {
+    Menu.find()
+        .select('_id name price ')
+        .exec()
+        .then(menu => {
+            const response = {
+                menuItem: menu.map(menu => {
+                    return {
+                        status: 200,
+                        message: "OK",
+                        data: [{
+                            _id: menu._id,                        
+                            name: menu.name,
+                            price: menu.price,
+                        }]
+                    }
+                })
+            };
+            
+            res.status(200).json(response);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error })
+        });
+});
+
 module.exports = router;
